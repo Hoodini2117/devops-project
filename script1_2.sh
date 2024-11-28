8e80dcc79f0ecfc396ff4d5439b147c4d8ae6bee
Hoodini2117 <143180769+Hoodini2117@users.noreply.github.com>
2024-11-28 23:01:48 +0530

    Update script1_2.sh

--- script1_2.sh
+++ script1_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "Top 5 CPU consuming processes:"
-ps aux --sort=-%cpu | head -6
-echo -e "\nTop 5 memory consuming processes:"
-ps aux --sort=-%mem | head -6
+echo "Top 10 CPU consuming processes:"
+ps aux --sort=-%cpu | head -10
+echo -e "\nTop 10 memory consuming processes:"
+ps aux --sort=-%mem | head -10
 echo -e "\nZombie processes:"
 ps aux | awk '$8=="Z" {print}'
